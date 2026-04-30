524bce444d13cae6eba19f7461c71533ac5ef6aa
likuuuado <lihuendaniel@gmail.com>
2026-04-30 01:26:51 -0300

    cambio de floats a radiales
    aca no hay receta, xq casi me pego 3 tiros intentando hacer que hacer que se forme el angulo y no una linea recta para arriba.
    la solucion: sacar el _on de _on_ready

--- Escenas/entidad_estatica.gd
+++ Escenas/entidad_estatica.gd
@@ -4,18 +4,24 @@ extends CharacterBody2D
 class_name Enemigos
 
 var player 
+var angleRads : float
 
 @export var angulo: float = 60
-@export var lenght: float
-var direction = Vector2.UP
+@export var lenght: float = 400
+@export var direction = Vector2.UP
 
-func _on_ready():
-	player = get_tree().get_nodes_in_group(player)
+func _ready():
+
+	angleRads = deg_to_rad(angulo / 2)
 
 
 func _draw():
-	var limIzquierda = direction.rotated(-0.6) * lenght
-	var limDerecha = direction.rotated(0.6) * lenght
+	var limIzquierda = direction.rotated(-angleRads) * lenght
+	var limDerecha = direction.rotated(angleRads) * lenght
 	
-	draw_line(Vector2.ZERO, limIzquierda, Color.RED, 2.5)
-	draw_line(Vector2.ZERO, limDerecha, Color.RED, 2.5)
+	draw_line(Vector2.ZERO, limIzquierda, Color.RED, 2.0)
+	draw_line(Vector2.ZERO, limDerecha, Color.GREEN, 2.0)
+
+func Deteccion():
+	var playerPos = to_local(player.global_position)
+	var anguloPlayer = direction.angle_to(playerPos)
