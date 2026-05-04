3b31ae60e2822fb92f2a64da63b8440c5e0b3e5f
likuuuado <lihuendaniel@gmail.com>
2026-05-04 19:54:29 -0300

    inputs

--- Escenas/player.gd
+++ Escenas/player.gd
@@ -4,6 +4,6 @@ class_name Jugador
 var speed = 100;
 
 func _physics_process(delta):
-	var direcciones = Input.get_vector("left", "right", "up", "down")
+	var direcciones = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direcciones * speed
 	move_and_slide()
