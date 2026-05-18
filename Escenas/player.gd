06cfe6445c716db3aa0aba6565fedf89bfa9d996
likuuuado <lihuendaniel@gmail.com>
2026-05-18 16:59:12 -0300

    añadido de luces y linterna gira pero mal

--- Escenas/player.gd
+++ Escenas/player.gd
@@ -1,9 +1,19 @@
 extends CharacterBody2D
 class_name Player
 
+
+@onready var linterna = $Linterna
+var posicionLinterna: Vector2
 var speed = 100;
+
+func _ready() -> void:
+	linterna.enabled = true
 
 func _physics_process(delta):
 	var direcciones = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direcciones * speed
+
+	posicionLinterna = get_viewport().get_mouse_position()
+	linterna.look_at(posicionLinterna)
+	
 	move_and_slide()
