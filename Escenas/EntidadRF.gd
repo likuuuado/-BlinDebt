5b0dffa14e0ae776e79763bced740d1285597580
likuuuado <lihuendaniel@gmail.com>
2026-05-18 22:51:29 -0300

    entidades movibles, con deteccion incluida
    nomas hay que configurarlas creando un path2d, dentro de ese un pathfollow y como hijo suyo la entidadRF.
    tambien se les puede poner una luz que se va a mover segun donde esten mirando

---
+++ Escenas/EntidadRF.gd
@@ -0,0 +1,88 @@
+extends CharacterBody2D
+
+
+class_name EnemigoRF
+
+var pathFollow
+var player
+
+@export var linternaVision: PointLight2D
+
+@export var color1: Color
+@export var color2: Color
+
+@export var tiempoDeteccion:  Timer
+
+@export var canvasPerder: CanvasLayer
+
+@export var tiempoAlerta: float
+var angleRads : float
+@export var angulo: float = 60
+@export var lenght: float = 400
+@export var speed: float
+
+@export var direction = Vector2.UP
+var pathPos: Vector2
+
+var detectando: bool = false
+
+func _ready():
+	pathFollow = get_parent()
+	pathPos = pathFollow.global_position 
+	player = get_tree().get_first_node_in_group("player")
+	angleRads = deg_to_rad(angulo / 2)
+	tiempoAlerta = 1.5
+	
+
+
+func _physics_process(delta):
+	if tiempoDeteccion.is_stopped():
+		pathFollow.progress += speed * delta
+		detectando = false
+		tiempoDeteccion.stop()
+
+
+
+func _process(delta):
+	if Deteccion():
+		tiempoDeteccion.wait_time = tiempoAlerta
+		if tiempoDeteccion.is_stopped():
+			tiempoDeteccion.start()
+	else:
+		tiempoDeteccion.wait_time = tiempoAlerta
+		detectando = false
+		tiempoDeteccion.stop()
+
+
+func _on_timer_timeout():
+
+	if $Sprite2D.modulate != Color.YELLOW && $Sprite2D.modulate != Color.RED:
+		$Sprite2D.modulate = Color.YELLOW
+		tiempoAlerta = 4
+		print("Enemigo alerta")
+		return
+	elif $Sprite2D.modulate == Color.YELLOW && $Sprite2D.modulate != Color.RED:
+		$Sprite2D.modulate = Color.RED
+	print("Jugador detectado")
+	canvasPerder.visible = true
+	get_tree().paused = true
+
+func _draw():
+	var limIzquierda = direction.rotated(-angleRads) * lenght
+	var limDerecha = direction.rotated(angleRads) * lenght
+
+	draw_line(Vector2.ZERO, limIzquierda, color1, 2.0)
+	draw_line(Vector2.ZERO, limDerecha, color2, 2.0)
+
+func Deteccion():
+	detectando = true
+	var playerPos = to_local(player.global_position)
+	var anguloPlayer = direction.angle_to(playerPos)
+	var distanciaPlayer = playerPos.length()
+	if distanciaPlayer > lenght:
+		return false
+
+	if abs(anguloPlayer) <= angleRads:
+		return true
+	else:
+		return false
